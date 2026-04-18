5dcde0e6cac70c6cc1f03bf80aaa4e0b56ee02e0
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-04-18 15:20:14 +0530

    PIE Single Emb model

--- models/VGG_PIE_SingleEmb.py
+++ models/VGG_PIE_SingleEmb.py
@@ -26,7 +26,8 @@ PI-CNN = CNN + Multi-view CNN
                     
 """
 
-from torchvision.models.vgg import model_urls as model_url_vgg
+# from torchvision.models.vgg import model_urls as model_url_vgg
+from torchvision.models import vgg16, VGG16_Weights
 import torch.nn.functional as F
 import torch
 import torch.nn as nn
@@ -41,7 +42,9 @@ class VGG_avg_pitc(nn.Module):
     def __init__(self, input_view, output_class):
         super(VGG_avg_pitc, self).__init__()
         self.input_view = input_view
-        VGG = models.vgg16(pretrained=True)
+        # model_url_vgg['vgg16'] = model_url_vgg['vgg16'].replace('https://', 'http://')
+        # VGG = models.vgg16(pretrained=True)
+        VGG = vgg16(weights=VGG16_Weights.DEFAULT)  # vgg16 weights
         VGG.classifier._modules['6'] = nn.Linear(
             4096, out_features=output_class)
 
@@ -78,3 +81,96 @@ class VGG_avg_pitc(nn.Module):
         return image_features, shape_feature, class_feature
 
 
+"""
+PI-Proxy with VGG backbone
+"""
+
+
+class VGG_avg_piproxy(nn.Module):
+    def __init__(self, input_view, output_class):
+        super(VGG_avg_piproxy, self).__init__()
+        self.input_view = input_view
+        VGG = vgg16(weights=VGG16_Weights.DEFAULT)
+        VGG = models.vgg16(pretrained=True)
+        VGG.classifier._modules['6'] = nn.Linear(
+            4096, out_features=output_class)
+
+        self.features = VGG.features
+        self.input_view = input_view
+        self.output_class = output_class
+        self.classifier1 = nn.Sequential(*list(VGG.classifier)[0:5])
+        self.class_centers = nn.Parameter(
+            # class center is a proxy
+            torch.randn(1, 1, self.output_class, 4096))
+
+    def forward(self, x):
+        image_features = None
+        shape_feature = None
+        _, views, _, _, _ = x.shape
+        for view in range(views):
+            view_feature = self.features(x[:, view]).view(x.shape[0], 25088)
+            view_feature = self.classifier1(
+                view_feature).view(x.shape[0], 1, 4096)
+
+            if image_features is None:
+                image_features = view_feature
+            else:
+                image_features = torch.cat([image_features, view_feature], 1)
+
+        class_feature = F.normalize(self.class_centers, p=2, dim=3, eps=1e-12)
+        class_feature = class_feature.view(self.output_class, 4096)
+
+        shape_feature = torch.mean(image_features, 1)
+        shape_feature = F.normalize(shape_feature, p=2, dim=1, eps=1e-12)
+
+        image_features = image_features.view(x.shape[0]*views, -1)
+        image_features = F.normalize(image_features, p=2, dim=1, eps=1e-12)
+
+        return image_features, shape_feature, class_feature
+
+
+"""
+PI-CNN model with VGG backbone
+"""
+
+
+class VGG_avg_picnn(nn.Module):
+    def __init__(self, input_view, output_class):
+        super(VGG_avg_picnn, self).__init__()
+        self.input_view = input_view
+        VGG = vgg16(weights=VGG16_Weights.DEFAULT)
+        VGG = models.vgg16(pretrained=True)
+        VGG.classifier._modules['6'] = nn.Linear(
+            4096, out_features=output_class)
+
+        self.features = VGG.features
+        self.input_view = input_view
+        self.output_class = output_class
+        self.classifier1 = nn.Sequential(*list(VGG.classifier)[0:5])
+        self.class_centers = nn.Parameter(
+            torch.randn(1, 1, self.output_class, 4096))
+
+    def forward(self, x):
+        image_features = None
+        shape_feature = None
+        _, views, _, _, _ = x.shape
+        for view in range(views):
+            view_feature = self.features(x[:, view]).view(x.shape[0], 25088)
+            view_feature = self.classifier1(
+                view_feature).view(x.shape[0], 1, 4096)
+
+            if image_features is None:
+                image_features = view_feature
+            else:
+                image_features = torch.cat([image_features, view_feature], 1)
+
+        class_feature = F.normalize(self.class_centers, p=2, dim=3, eps=1e-12)
+        class_feature = class_feature.view(self.output_class, 4096)
+
+        shape_feature = torch.mean(image_features, 1)
+        shape_feature = F.normalize(shape_feature, p=2, dim=1, eps=1e-12)
+
+        image_features = image_features.view(x.shape[0]*views, -1)
+        image_features = F.normalize(image_features, p=2, dim=1, eps=1e-12)
+
+        return image_features, shape_feature, class_feature
